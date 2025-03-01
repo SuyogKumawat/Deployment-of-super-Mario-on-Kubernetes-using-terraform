78e50c31614f11a953cdb6db5977aafbf3a14e8d
Suyog Kumawat <67118277+SuyogKumawat@users.noreply.github.com>
2025-03-01 21:03:43 +0530

    Update provider.tf

--- EKS-TF/provider.tf
+++ EKS-TF/provider.tf
@@ -7,7 +7,10 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
+# Configure the AWS Provider and region you can specify
+#If you launch using EC2 then no need to put access key and secret key
+
+
 provider "aws" {
   region = "ap-south-1"
 }
